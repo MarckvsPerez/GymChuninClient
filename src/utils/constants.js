eaa3599996ff2d5f2a128dc11faf73996d7f323f
Marc Pérez Salat <marcantoniperez@gmail.com>
2023-12-18 21:39:50 +0100

    congif

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,9 @@
-const SERVER_IP = "gymchuninserver-production.up.railway.app";
+const SERVER_IP = "localhost:3977";
+// const SERVER_IP = "gymchuninserver-production.up.railway.app";
 
 export const ENV = {
-  BASE_PATH: `https://${SERVER_IP}`,
-  BASE_API: `https://${SERVER_IP}/api/v1`,
+  BASE_PATH: `http://${SERVER_IP}`,
+  BASE_API: `http://${SERVER_IP}/api/v1`,
   API_ROUTES: {
     REGISTER: "auth/register",
     LOGIN: "auth/login",
